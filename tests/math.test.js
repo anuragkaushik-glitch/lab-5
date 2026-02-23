38297a809569993fe695e872cd2e267be94a2c74
Yashsingh045 <astomar6396@gmail.com>
2026-02-23 18:41:22 +0530

    Revert "testing on pull_request on main"
    This reverts commit a026a769e232a7665becd3aaf90a28fef7377ad1.

--- tests/math.test.js
+++ tests/math.test.js
@@ -3,7 +3,7 @@ import assert from "node:assert/strict";
 import { add, isEven } from "../src/math.js";
 
 test("add adds numbers", () => {
-  assert.equal(add(2, 3), 6);
+  assert.equal(add(2, 3), 5);
 });
 
 test("isEven detects even numbers", () => {
